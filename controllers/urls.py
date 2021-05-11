93706a93f98758adbb7fff41833cad3b2a20ecda
Elwin <elwin.carlos09@gmail.com>
2021-05-11 17:00:15 +0100

    change error type to NotFound

--- controllers/urls.py
+++ controllers/urls.py
@@ -1,5 +1,5 @@
 import app
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, NotFound
 
 def create(url_short, url_long):
   app.query_db('insert into urls (id, url) values (?, ?);', (url_short, url_long))
@@ -9,8 +9,8 @@ def find_by_id(id):
   try:
     url = app.query_db('select url from urls where id = (?);', (id,))
     if not url:
-      raise BadRequest(f"This short URL {id} does not lead anywhere :(  Try again, and include http:// in your request.")
+      raise NotFound(f"This short URL {id} does not lead anywhere :(  Try again, and include http:// in your request.")
     else:
       return url
   except:
-    raise BadRequest(f"This short URL {id} does not lead anywhere :(  Try again, and include http:// in your request.")
+    raise NotFound(f"This short URL {id} does not lead anywhere :(  Try again, and include http:// in your request.")
